bc4c16308f8d2dfde70e766832ffff06bbb0bac9
Sam Johnson <sam@durosoft.com>
2023-07-23 02:02:45 -0400

    basic macro setup

--- src/lib.rs
+++ src/lib.rs
@@ -1,1 +1,16 @@
+use proc_macro::TokenStream;
+use proc_macro2::TokenStream as TokenStream2;
+use quote::quote;
+use syn::Result;
 
+#[proc_macro]
+pub fn gpt(tokens: TokenStream) -> TokenStream {
+    match gpt_internal(tokens) {
+        Ok(tokens) => tokens.into(),
+        Err(err) => err.into_compile_error().into(),
+    }
+}
+
+fn gpt_internal(tokens: impl Into<TokenStream2>) -> Result<TokenStream2> {
+    Ok(quote!())
+}
